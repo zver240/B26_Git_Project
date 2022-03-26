bd2a86f20a76045817cb6d95d72c7d27a26702e8
alinajob@aol.com <Amira3828>
2022-03-26 16:10:50 -0400

    new updated first file

--- src/Day0/First.java
+++ src/Day0/First.java
@@ -1,8 +1,10 @@
 package Day0;
 
 public class First {
-    //TODO: Work on this tomorrow
-    //add some cool code
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("Working...");
+    }
     //extra line 1
     //extra line2
     //extra line 3
